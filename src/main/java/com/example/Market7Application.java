2c5d07e60781649eb008ce0198a0df3b3f955ddf
alexn34-n <alexn3469@mail.ru>
2021-06-07 09:38:06 +0300

    Initial commit

---
+++ src/main/java/com/example/Market7Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Market7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Market7Application.class, args);
+    }
+
+}
